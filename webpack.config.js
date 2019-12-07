445cd1ec35f1494bc36da598305089fc009ff5e2
mmyenny <mmy491@gmail.com>
2019-12-06 21:46:59 -0500

    :)

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,36 @@
-const webpack = require('webpack')
+const webpack = require("webpack");
 
 plugins: [
   new webpack.ProvidePlugin({
-    mapboxgl: 'mapbox-gl',
-  }),
-]
+    mapboxgl: "mapbox-gl"
+  })
+];
+
+module.exports = {
+  rules: [
+    {
+      test: /\.s(c|a)ss$/,
+      use: [
+        "vue-style-loader",
+        "css-loader",
+        {
+          loader: "sass-loader",
+          // Requires sass-loader@^7.0.0
+          options: {
+            implementation: require("sass"),
+            fiber: require("fibers"),
+            indentedSyntax: true // optional
+          },
+          // Requires sass-loader@^8.0.0
+          options: {
+            implementation: require("sass"),
+            sassOptions: {
+              fiber: require("fibers"),
+              indentedSyntax: true // optional
+            }
+          }
+        }
+      ]
+    }
+  ]
+};
